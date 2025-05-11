37da680f33fc69abe7244a798e7b0ab8abbf0b2c
mcieslik <01187214@pw.edu.pl>
2025-05-11 15:55:23 +0200

    Deleted unnecessary code, changed to adnotations

--- backend/src/main/java/com/bd2_team6/biteright/entities/daily_summary/DailySummaryId.java
+++ backend/src/main/java/com/bd2_team6/biteright/entities/daily_summary/DailySummaryId.java
@@ -1,37 +1,19 @@
 package com.bd2_team6.biteright.entities.daily_summary;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor
 /// Class representing the double primary key of the DailySummary entity
-public class DailySummaryId {
+public class DailySummaryId implements Serializable {
     @Column(name = "user_id")
-    private Integer user_id;
+    private Integer userId;
     
     @Column(name = "summary_date")
-    private Date summary_date;
-
-
-
-    // CONSTRUCTOR
-    public DailySummaryId() {}
-
-    // EQUALS AND HASHCODE METHODS
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DailySummaryId that = (DailySummaryId) o;
-        return  Objects.equals(user_id, that.user_id) && 
-                Objects.equals(summary_date, that.summary_date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(user_id, summary_date);
-    }
-
-
+    private Date summaryDate;
 }
