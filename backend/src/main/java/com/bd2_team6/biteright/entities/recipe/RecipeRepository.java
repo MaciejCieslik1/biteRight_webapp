e1aa03743da34173c103cfd9b8c6cd0272e82e88
mcieslik <01187214@pw.edu.pl>
2025-05-14 21:57:52 +0200

    Changed crudRepository to JpaRepository

--- backend/src/main/java/com/bd2_team6/biteright/entities/recipe/RecipeRepository.java
+++ backend/src/main/java/com/bd2_team6/biteright/entities/recipe/RecipeRepository.java
@@ -1,9 +1,10 @@
 package com.bd2_team6.biteright.entities.recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RecipeRepository extends CrudRepository<Recipe, Integer>{
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     
 }
