e8ccd95b03a411e2790a3e11b238c0bd6bafd51e
annawierzbik <annawierzbik1@gmail.com>
2025-05-14 10:18:03 +0200

    added styles for calendar

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import NavBar from "../../components/NavBar";
 import Calendar from "../../components/Calendar";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
@@ -8,8 +9,10 @@ const HomePage = () => {
       <div className="home-page-container">
         <h1>Welcome to the Home Page</h1>
         <p>This is the content of the home page.</p>
+        <div className="calendar-container">
+          <Calendar />
+        </div>
       </div>
-      <Calendar />
     </div>
   );
 };
