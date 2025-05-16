1a1f099ac491936ba39c12f82cd2094f3d30d269
mcieslik <01187214@pw.edu.pl>
2025-05-16 13:06:46 +0200

    Changed GET to POST method in update

--- backend/src/main/java/com/bd2_team6/biteright/controllers/UserInfoController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/UserInfoController.java
@@ -6,10 +6,7 @@ import com.bd2_team6.biteright.service.UserInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/userInfo")
@@ -31,7 +28,7 @@ public class UserInfoController {
         }
     }
 
-    @GetMapping("/updateUserInfo")
+    @PostMapping("/updateUserInfo")
     public ResponseEntity<?> updateUserInfo(Authentication authentication, @RequestBody UserInfoUpdateRequest request) {
         String username = authentication.getName();
 
