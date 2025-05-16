3613993c2a9ae3233368094c9c11826f0b862f96
mcieslik <01187214@pw.edu.pl>
2025-05-16 22:32:07 +0200

    Fixed tests issues

--- backend/src/test/java/com/bd2_team6/biteright/BiteRightApplicationTests.java
+++ backend/src/test/java/com/bd2_team6/biteright/BiteRightApplicationTests.java
@@ -2,8 +2,10 @@ package com.bd2_team6.biteright;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BiteRightApplicationTests {
 
 	@Test
