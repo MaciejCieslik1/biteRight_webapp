1232ebdd5690175f1909e64e58135ab35b42cb7c
mcieslik <01187214@pw.edu.pl>
2025-05-16 13:00:23 +0200

    Added UserInfoController

---
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/UserInfoController.java
@@ -0,0 +1,46 @@
+package com.bd2_team6.biteright.controllers;
+
+import com.bd2_team6.biteright.controllers.requests.update_requests.UserInfoUpdateRequest;
+import com.bd2_team6.biteright.entities.user_info.UserInfo;
+import com.bd2_team6.biteright.service.UserInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userInfo")
+@RequiredArgsConstructor
+public class UserInfoController {
+
+    private final UserInfoService userInfoService;
+
+    @GetMapping("/findUserInfo")
+    public ResponseEntity<?> findUserInfo(Authentication authentication) {
+        String username = authentication.getName();
+
+        try {
+            UserInfo userInfo = userInfoService.findUserInfoByUsername(username);
+            return ResponseEntity.ok(userInfo);
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/updateUserInfo")
+    public ResponseEntity<?> updateUserInfo(Authentication authentication, @RequestBody UserInfoUpdateRequest request) {
+        String username = authentication.getName();
+
+        try {
+            UserInfo userInfo = userInfoService.updateUserInfo(username, request);
+            return ResponseEntity.ok(userInfo);
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
