72d600804dd2e7b65d5fb4ec0497ebf9e6ff9f3e
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-17 16:53:48 +0200

    Added find endpoint for DailySummary

--- backend/src/main/java/com/bd2_team6/biteright/controllers/DailySummaryController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/DailySummaryController.java
@@ -1,5 +1,31 @@
 package com.bd2_team6.biteright.controllers;
 
+import com.bd2_team6.biteright.entities.daily_summary.DailySummary;
+import com.bd2_team6.biteright.service.DailySummaryService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/dailySummary")
+@RequiredArgsConstructor
 public class DailySummaryController {
-    
+    private final DailySummaryService dailySummaryService;
+
+    @GetMapping("/findDailySummary")
+    public ResponseEntity<?> findDailySummary(Authentication authentication) {
+        String username = authentication.getName();
+
+        try {
+            DailySummary dailySummary = dailySummaryService.findDailySummaryByUsername(username);
+            return ResponseEntity.ok(dailySummary);
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- backend/src/main/java/com/bd2_team6/biteright/service/DailySummaryService.java
+++ backend/src/main/java/com/bd2_team6/biteright/service/DailySummaryService.java
@@ -1,5 +1,31 @@
 package com.bd2_team6.biteright.service;
 
+import com.bd2_team6.biteright.entities.daily_limits.DailyLimitsRepository;
+import com.bd2_team6.biteright.entities.daily_summary.DailySummary;
+import com.bd2_team6.biteright.entities.daily_summary.DailySummaryRepository;
+import com.bd2_team6.biteright.entities.user.User;
+import com.bd2_team6.biteright.entities.user.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DailySummaryService {
-    
+    private final UserRepository userRepository;
+    private final DailySummaryRepository dailySummaryRepository;
+
+    @Autowired
+    public DailySummaryService(UserRepository userRepository, DailySummaryRepository dailySummaryRepository) {
+        this.userRepository = userRepository;
+        this.dailySummaryRepository = dailySummaryRepository;
+    }
+
+    public DailySummary findDailySummaryByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Integer user_id = user.getId();
+        DailySummary summary = dailySummaryRepository.findById(user_id)
+                .orElseThrow(() -> new IllegalArgumentException("Summary not found"));
+        return summary;
+    }
 }
