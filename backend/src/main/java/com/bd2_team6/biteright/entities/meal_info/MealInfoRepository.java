b7a080adb416049f210008ae2fed1528c68809b2
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-18 23:29:47 +0200

    Fix for meal info

--- backend/src/main/java/com/bd2_team6/biteright/entities/meal_info/MealInfoRepository.java
+++ backend/src/main/java/com/bd2_team6/biteright/entities/meal_info/MealInfoRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MealInfoRepository extends JpaRepository<MealInfo, Integer> {
-    Optional<MealInfo> findByName(String mealName);
+    Optional<MealInfo> findByMealName(String mealName);
 }
--- backend/src/main/java/com/bd2_team6/biteright/service/MealInfoService.java
+++ backend/src/main/java/com/bd2_team6/biteright/service/MealInfoService.java
@@ -1,7 +1,5 @@
 package com.bd2_team6.biteright.service;
 
-import com.bd2_team6.biteright.entities.meal.Meal;
-import com.bd2_team6.biteright.entities.meal.MealRepository;
 import com.bd2_team6.biteright.entities.meal_info.MealInfo;
 import com.bd2_team6.biteright.entities.meal_info.MealInfoRepository;
 
@@ -24,7 +22,7 @@ public class MealInfoService {
     }
 
     public MealInfo findMealInfoByName(String mealName) {
-        MealInfo mealInfo = mealInfoRepository.findByName(mealName)
+        MealInfo mealInfo = mealInfoRepository.findByMealName(mealName)
                 .orElseThrow(() -> new IllegalArgumentException("Meal info not found"));
         return mealInfo;
     }
