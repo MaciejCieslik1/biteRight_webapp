843e025acbc62693b13af0824c54f7f7dc2e4b59
Hanna Biegacz <biegacz.hanna@gmail.com>
2025-05-18 13:06:18 +0200

    made customuserdetailsservice a component

--- backend/src/main/java/com/bd2_team6/biteright/authentication/custom_user_details/CustomUserDetailsService.java
+++ backend/src/main/java/com/bd2_team6/biteright/authentication/custom_user_details/CustomUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bd2_team6.biteright.entities.user.UserRepository;
 import com.bd2_team6.biteright.entities.user.User;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Component
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
