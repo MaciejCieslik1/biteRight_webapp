d5a5b0cdf2d2d316a0f6bb17a1e7310088c6cc30
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-18 23:25:34 +0200

    Added option to update daily limits

--- backend/src/main/java/com/bd2_team6/biteright/controllers/DailyLimitsController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/DailyLimitsController.java
@@ -1,5 +1,6 @@
 package com.bd2_team6.biteright.controllers;
 
+import com.bd2_team6.biteright.controllers.requests.update_requests.DailyLimitsUpdateRequest;
 import com.bd2_team6.biteright.entities.daily_limits.DailyLimits;
 import com.bd2_team6.biteright.service.DailyLimitsService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/dailyLimits")
@@ -15,13 +17,26 @@ import org.springframework.web.bind.annotation.*;
 public class DailyLimitsController {
     private final DailyLimitsService dailyLimitsService;
 
-    @GetMapping("/findDailyLimits")
+    @GetMapping("/find")
     public ResponseEntity<?> findDailyLimits(Authentication authentication) {
         String username = authentication.getName();
 
         try {
             DailyLimits dailyLimits = dailyLimitsService.findDailyLimitsByUsername(username);
             return ResponseEntity.ok(dailyLimits);
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateDailyLimits(Authentication authentication, @RequestBody DailyLimitsUpdateRequest request) {
+        String username = authentication.getName();
+
+        try {
+            DailyLimits updatedDailyLimits = dailyLimitsService.updateDailyLimits(username, request);
+            return ResponseEntity.ok(updatedDailyLimits);
         }
         catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();
--- backend/src/main/java/com/bd2_team6/biteright/service/DailyLimitsService.java
+++ backend/src/main/java/com/bd2_team6/biteright/service/DailyLimitsService.java
@@ -1,5 +1,6 @@
 package com.bd2_team6.biteright.service;
 
+import com.bd2_team6.biteright.controllers.requests.update_requests.DailyLimitsUpdateRequest;
 import com.bd2_team6.biteright.entities.daily_limits.DailyLimits;
 import com.bd2_team6.biteright.entities.daily_limits.DailyLimitsRepository;
 import com.bd2_team6.biteright.entities.user.User;
@@ -23,5 +24,19 @@ public class DailyLimitsService {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         return user.getDailyLimits();
+    }
+
+    public DailyLimits updateDailyLimits(String username, DailyLimitsUpdateRequest request) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        DailyLimits dailyLimits = user.getDailyLimits();
+        dailyLimits.setCalorieLimit(request.getCalorieLimit());
+        dailyLimits.setProteinLimit(request.getCalorieLimit());
+        dailyLimits.setFatLimit(request.getFatLimit());
+        dailyLimits.setCarbLimit(request.getCarbLimit());
+        dailyLimits.setWaterGoal(request.getWaterGoal());
+        dailyLimitsRepository.save(dailyLimits);
+
+        return dailyLimits;
     }
 }
