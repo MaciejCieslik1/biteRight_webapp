691ce21a700edd9c773a0324cc13a4a50e6e38d2
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-18 23:04:49 +0200

    Added User controller and service

--- backend/src/main/java/com/bd2_team6/biteright/controllers/AddressController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/AddressController.java
@@ -12,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-
-
 @RestController
 @RequestMapping("/address")
 @RequiredArgsConstructor
