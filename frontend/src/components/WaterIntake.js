b50020a8036a41a1fd976cdafd00ed6f686ac038
Anna Wierzbik <01186956@pw.edu.pl>
2025-05-18 17:58:48 +0200

    added water intake section

---
+++ frontend/src/components/WaterIntake.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WaterIntake.css";
+
+const WaterIntake = () => {
+  return (
+    <div className="water-intake">
+      <div className="water-intake-header"> Water intake </div>
+      <div className="water-intake-body"> Water </div>
+      <div className="water-intake-footer"> Amount / 2 liters </div>
+    </div>
+  );
+};
+export default WaterIntake;
