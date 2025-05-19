d30ffb9c36ed52c81314624e4a405bf63d3d3e53
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-19 22:29:45 +0200

    Addedoption to create and update ExerciseInfo

--- backend/src/main/java/com/bd2_team6/biteright/controllers/ExerciseInfoController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/ExerciseInfoController.java
@@ -1,5 +1,7 @@
 package com.bd2_team6.biteright.controllers;
 
+import com.bd2_team6.biteright.controllers.requests.create_requests.ExerciseInfoCreateRequest;
+import com.bd2_team6.biteright.controllers.requests.update_requests.ExerciseInfoUpdateRequest;
 import com.bd2_team6.biteright.entities.exercise_info.ExerciseInfo;
 import com.bd2_team6.biteright.service.ExerciseInfoService;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,31 @@ import org.springframework.web.bind.annotation.*;
 public class ExerciseInfoController {
     private final ExerciseInfoService exerciseInfoService;
 
-    @GetMapping("/find")
-    public ResponseEntity<?> getExerciseInfoByName(@RequestParam String name) {
+    @GetMapping("/find/{name}")
+    public ResponseEntity<?> findExerciseInfo(@PathVariable("name") String exerciseName) {
         try {
-            ExerciseInfo info = exerciseInfoService.findByName(name);
+            ExerciseInfo info = exerciseInfoService.findExerciseInfoByName(exerciseName);
             return ResponseEntity.ok(info);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createExerciseInfo(@RequestBody ExerciseInfoCreateRequest request){
+        try {
+            ExerciseInfo newInfo = exerciseInfoService.createExerciseInfo(request);
+            return ResponseEntity.ok(newInfo);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/update/{name}") 
+    public ResponseEntity<?> updateExerciseInfo(@PathVariable("name") String exerciseName, @RequestBody ExerciseInfoUpdateRequest request){
+        try {
+            ExerciseInfo updatedInfo = exerciseInfoService.updateExerciseInfo(exerciseName, request);
+            return ResponseEntity.ok(updatedInfo);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();
         }
--- backend/src/main/java/com/bd2_team6/biteright/service/ExerciseInfoService.java
+++ backend/src/main/java/com/bd2_team6/biteright/service/ExerciseInfoService.java
@@ -1,5 +1,7 @@
 package com.bd2_team6.biteright.service;
 
+import com.bd2_team6.biteright.controllers.requests.create_requests.ExerciseInfoCreateRequest;
+import com.bd2_team6.biteright.controllers.requests.update_requests.ExerciseInfoUpdateRequest;
 import com.bd2_team6.biteright.entities.exercise_info.ExerciseInfo;
 import com.bd2_team6.biteright.entities.exercise_info.ExerciseInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +17,23 @@ public class ExerciseInfoService {
         this.exerciseInfoRepository = exerciseInfoRepository;
     }
 
-    public ExerciseInfo findByName(String name) {
+    public ExerciseInfo findExerciseInfoByName(String name) {
         return exerciseInfoRepository.findByName(name)
                 .orElseThrow(() -> new IllegalArgumentException("Exercise with name '" + name + "' not found"));
+    }
+
+    public ExerciseInfo createExerciseInfo(ExerciseInfoCreateRequest request) {
+        ExerciseInfo newInfo = new ExerciseInfo();
+        newInfo.setMetabolicEquivalent(request.getMetabolicEquivalent());
+        newInfo.setName(request.getName());
+        return exerciseInfoRepository.save(newInfo);
+    }
+
+    public ExerciseInfo updateExerciseInfo(String name, ExerciseInfoUpdateRequest request) {
+        ExerciseInfo newInfo = exerciseInfoRepository.findByName(name)
+                .orElseThrow(() -> new IllegalArgumentException("Exercise with name '" + name + "' not found"));
+        newInfo.setMetabolicEquivalent(request.getMetabolicEquivalent());
+        newInfo.setName(request.getName());
+        return exerciseInfoRepository.save(newInfo);
     }
 }
