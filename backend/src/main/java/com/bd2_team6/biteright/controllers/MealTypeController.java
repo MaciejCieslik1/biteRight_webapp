24a6fa2e4c1da8a17e30942ee8d89d08dda7c572
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-21 19:55:19 +0200

    Fixed find endpoint for mealType

--- backend/src/main/java/com/bd2_team6/biteright/controllers/MealTypeController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/MealTypeController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.bd2_team6.biteright.controllers.DTO.MealTypeDTO;
 import com.bd2_team6.biteright.entities.meal_type.MealType;
 import com.bd2_team6.biteright.service.MealTypeService;
 
@@ -14,22 +15,24 @@ import com.bd2_team6.biteright.service.MealTypeService;
 public class MealTypeController {
     private final MealTypeService mealTypeService;
 
-    @GetMapping("/findByName/{id}")
+    @GetMapping("/findById/{id}")
     public ResponseEntity<?> findMealTypeById(@PathVariable("id") Integer mealId) {
         try {
             MealType mealType = mealTypeService.findMealTypeById(mealId);
-            return ResponseEntity.ok(mealType);
+            MealTypeDTO mealTypeDTO = new MealTypeDTO(mealType.getTypeId(), mealType.getName());
+            return ResponseEntity.ok(mealTypeDTO);
         }
         catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @GetMapping("/findById/{name}")
+    @GetMapping("/findByName/{name}")
     public ResponseEntity<?> findMealTypeByName(@PathVariable("name") String mealTypeName) {
         try {
             MealType mealType = mealTypeService.findMealTypeByName(mealTypeName);
-            return ResponseEntity.ok(mealType);
+            MealTypeDTO mealTypeDTO = new MealTypeDTO(mealType.getTypeId(), mealType.getName());
+            return ResponseEntity.ok(mealTypeDTO);
         }
         catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();
