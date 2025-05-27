54ce1cc085f61a0a6b6fd3f81a70750726b925d0
Hanna Biegacz <biegacz.hanna@gmail.com>
2025-05-27 13:55:51 +0200

    removed redundant import --- authController

--- backend/src/main/java/com/bd2_team6/biteright/controllers/AuthenticationController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.bd2_team6.biteright.controllers.requests.RegistrationRequestBody;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
 
