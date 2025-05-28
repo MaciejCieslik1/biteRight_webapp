ab4b786a697ea33c2129836de023175a0b44ec05
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-28 22:20:40 +0200

    Added ingredientName to RecipeContentDTO

--- backend/src/main/java/com/bd2_team6/biteright/controllers/DTO/RecipeContentDTO.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/DTO/RecipeContentDTO.java
@@ -11,10 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RecipeContentDTO {
     private Integer ingredientId;
+    private String ingredientName;
     private Integer ingredientAmount;
 
     public RecipeContentDTO(RecipeContent content) {
         this.ingredientId = content.getIngredient().getIngredientId();
+        this.ingredientName = content.getIngredient().getName();
         this.ingredientAmount = content.getIngredientAmount();
     }
 }
