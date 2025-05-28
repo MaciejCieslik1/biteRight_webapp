020f4a580d2625f9f2f1dda3018943b3f9356abe
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-28 21:20:54 +0200

    Create RecipeController and fixed find endpoints for meal

--- backend/src/main/java/com/bd2_team6/biteright/controllers/RecipeController.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/RecipeController.java
@@ -1,31 +1,74 @@
 package com.bd2_team6.biteright.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import com.bd2_team6.biteright.controllers.requests.create_requests.RecipeCreateRequest;
+import com.bd2_team6.biteright.controllers.requests.update_requests.RecipeUpdateRequest;
 import com.bd2_team6.biteright.entities.recipe.Recipe;
+import com.bd2_team6.biteright.entities.user.UserRepository;
 import com.bd2_team6.biteright.service.RecipeService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/recipe")
 @RequiredArgsConstructor
 public class RecipeController {
     private final RecipeService recipeService;
+    private final UserRepository userRepository;
 
-    @GetMapping("/find/{name}")
-    public ResponseEntity<?> findRecipeName(@PathVariable("name") String name) {
+    @GetMapping("/findByName/{name}")
+    public ResponseEntity<?> findRecipeByName(Authentication authentication, @PathVariable("name") String recipeName) {
         try {
-            Recipe recipe = recipeService.findRecipeByName(name);
+            Recipe recipe = recipeService.findRecipeByName(recipeName);
             return ResponseEntity.ok(recipe);
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();
         }
     }
-    
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<?> findRecipeByName(Authentication authentication, @PathVariable("id") Integer recipeId) {
+        try {
+            Recipe recipe = recipeService.findRecipeByName(recipeId);
+            return ResponseEntity.ok(recipe);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createRecipe(Authentication authentication, @RequestBody RecipeCreateRequest request) {
+        try {
+            Recipe recipe = recipeService.createRecipe(request);
+            return ResponseEntity.ok(recipe);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateRecipe(Authentication authentication, @RequestBody RecipeUpdateRequest request,
+                                          @PathVariable("id") Integer recipeId) {
+        try {
+            Recipe updated = recipeService.updateRecipe(request, recipeId);
+            return ResponseEntity.ok(updated);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteRecipe(Authentication authentication, @PathVariable("id") Integer recipeId) {
+        try {
+            recipeService.deleteRecipe(recipeId);
+            return ResponseEntity.ok("Recipe successfully deleted");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
