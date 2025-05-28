d6f8a14aaa919cc9372f9dacbead70763415ff1e
Michał Iwanow-Kołakowski <01186947@pw.edu.pl>
2025-05-28 21:45:07 +0200

    Created RecipeService

--- backend/src/main/java/com/bd2_team6/biteright/service/RecipeService.java
+++ backend/src/main/java/com/bd2_team6/biteright/service/RecipeService.java
@@ -1,21 +1,80 @@
 package com.bd2_team6.biteright.service;
 
-import org.springframework.stereotype.Service;
-
+import com.bd2_team6.biteright.controllers.DTO.RecipeContentDTO;
+import com.bd2_team6.biteright.controllers.requests.create_requests.RecipeCreateRequest;
+import com.bd2_team6.biteright.controllers.requests.update_requests.RecipeUpdateRequest;
+import com.bd2_team6.biteright.entities.ingredient.Ingredient;
+import com.bd2_team6.biteright.entities.ingredient.IngredientRepository;
 import com.bd2_team6.biteright.entities.recipe.Recipe;
 import com.bd2_team6.biteright.entities.recipe.RecipeRepository;
+import com.bd2_team6.biteright.entities.recipe_content.RecipeContent;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RecipeService {
     private final RecipeRepository recipeRepository;
-
-    public RecipeService(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
+    private final IngredientRepository ingredientRepository;
 
     public Recipe findRecipeByName(String name) {
-        Recipe recipe = recipeRepository.findByName(name)
+        return recipeRepository.findByName(name)
                 .orElseThrow(() -> new IllegalArgumentException("Recipe not found"));
-        return recipe;
+    }
+
+    public Recipe findRecipeById(Integer recipeId) {
+        return recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new IllegalArgumentException("Recipe not found"));
+    }
+
+    public Recipe createRecipe(RecipeCreateRequest request) {
+        Recipe recipe = new Recipe();
+        recipe.setName(request.getName());
+        recipe.setDescription(request.getDescription());
+
+        for (RecipeContentDTO dto : request.getContents()) {
+            Ingredient ingredient = ingredientRepository.findById(dto.getIngredientId())
+                    .orElseThrow(() -> new IllegalArgumentException("Ingredient not found"));
+
+            RecipeContent content = new RecipeContent();
+            content.setIngredient(ingredient);
+            content.setIngredientAmount(dto.getIngredientAmount());
+            content.setRecipe(recipe);
+
+            recipe.getRecipeContents().add(content);
+        }
+
+        return recipeRepository.save(recipe);
+    }
+
+    public Recipe updateRecipe(RecipeUpdateRequest request, Integer recipeId) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new IllegalArgumentException("Recipe not found"));
+
+        recipe.setName(request.getName());
+        recipe.setDescription(request.getDescription());
+
+        recipe.getRecipeContents().clear();
+
+        for (RecipeContentDTO dto : request.getContents()) {
+            Ingredient ingredient = ingredientRepository.findById(dto.getIngredientId())
+                    .orElseThrow(() -> new IllegalArgumentException("Ingredient not found"));
+
+            RecipeContent content = new RecipeContent();
+            content.setIngredient(ingredient);
+            content.setIngredientAmount(dto.getIngredientAmount());
+            content.setRecipe(recipe);
+
+            recipe.getRecipeContents().add(content);
+        }
+
+        return recipeRepository.save(recipe);
+    }
+
+    public void deleteRecipe(Integer recipeId) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new IllegalArgumentException("Recipe not found"));
+        recipeRepository.delete(recipe);
     }
 }
