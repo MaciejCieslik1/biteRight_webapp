c0e24545c11f1f1a686d41be13170d2e2d654bd2
Hanna Biegacz <biegacz.hanna@gmail.com>
2025-05-29 13:48:35 +0200

    request classes rename

--- backend/src/main/java/com/bd2_team6/biteright/controllers/requests/RegistrationRequest.java
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/requests/RegistrationRequest.java
@@ -1,10 +1,12 @@
 package com.bd2_team6.biteright.controllers.requests;
+
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class LoginRequestBody {
-    private String email;
+public class RegistrationRequest {
+    private String username;
     private String password;
+    private String email;
 }
