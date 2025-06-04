07c7be75bfc9691ff51b72bdb30f8394f29beb4b
annawierzbik <annawierzbik1@gmail.com>
2025-06-04 21:13:30 +0200

    added footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.css";
+import { FaHome, FaReadme, FaCog } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <footer className="footer">
+      <div className="footer-buttons">
+        <button
+          className="footer-btn"
+          title="Home"
+          onClick={() => navigate("/home")}
+        >
+          <FaHome size={20} />
+        </button>
+        <button className="footer-btn" title="Guide">
+          <FaReadme size={20} />
+        </button>
+        <button
+          className="footer-btn"
+          title="Settings"
+          onClick={() => navigate("/settings")}
+        >
+          <FaCog size={20} />
+        </button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
