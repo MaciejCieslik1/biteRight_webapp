243d2bce05c5db1c1f17e3bf044f05f8e7d98ad7
mcieslik <01187214@pw.edu.pl>
2025-06-04 17:18:23 +0200

    Added DTO class

---
+++ backend/src/main/java/com/bd2_team6/biteright/controllers/DTO/LimitHistoryDTO.java
@@ -0,0 +1,20 @@
+package com.bd2_team6.biteright.controllers.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LimitHistoryDTO {
+    private Integer historyId;
+    private LocalDate dateChanged;
+    private Integer calorieLimit;
+    private Integer proteinLimit;
+    private Integer fatLimit;
+    private Integer carbLimit;
+    private Integer waterGoal;
+}
