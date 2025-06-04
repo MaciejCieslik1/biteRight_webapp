96d7dcaa9993560398b4b8d9daff381bc0aada9e
Hanna Biegacz <biegacz.hanna@gmail.com>
2025-06-05 01:52:06 +0200

    created trigger for inserted and updated calories

--- database/database_2_functions.sql
+++ database/database_2_functions.sql
@@ -51,4 +51,35 @@ BEGIN
     SET NEW.bmi = NEW.weight / (POWER(NEW.height / 100, 2));
 END $$
 
+CREATE TRIGGER CALC_CALORIES_BURNT
+BEFORE INSERT ON user_exercise
+FOR EACH ROW 
+BEGIN
+    DECLARE user_weight DECIMAL(5,2);
+    DECLARE exercise_met DECIMAL(5,2);
+
+    SELECT weight INTO user_weight FROM user_info WHERE user_id = NEW.user_id;
+    SELECT metabolic_equivalent INTO exercise_met FROM exercise_info WHERE exercise_id = NEW.exercise_id;
+
+	SET NEW.calories_burnt = ROUND(exercise_met * user_weight * NEW.duration / 60);
+END $$
+
+CREATE TRIGGER CALC_CALORIES_BURNT_UPDATE
+BEFORE UPDATE ON user_exercise
+FOR EACH ROW 
+BEGIN
+    DECLARE user_weight DECIMAL(5,2);
+    DECLARE exercise_met DECIMAL(5,2);
+
+    SELECT weight INTO user_weight
+    FROM user_info
+    WHERE user_id = NEW.user_id;
+
+    SELECT metabolic_equivalent INTO exercise_met
+    FROM exercise_info
+    WHERE exercise_id = NEW.exercise_id;
+
+    SET NEW.calories_burnt = ROUND(exercise_met * user_weight * NEW.duration / 60);
+END $$
+
 DELIMITER ;
